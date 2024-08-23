7df6c9d23e89d0314227d91929e189466d4401a0
Usuario <Usuario@DESKTOP-15SCPVC>
2024-08-22 22:47:31 -0600

    Semana 13: GoogleCloud

--- src/main/java/com/tienda/ProjectConfig.java
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -25,6 +25,8 @@ import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 
 @Configuration
@@ -129,4 +131,16 @@ public class ProjectConfig  implements WebMvcConfigurer{
                 .passwordEncoder(new BCryptPasswordEncoder());
         
     }
+    /* El siguiente método se utilizar para publicar en la nube, independientemente  */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    } 
+
 }
